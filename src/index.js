8b3aac75a1d1d42a426e8386f49e00fef3a885a6
Mariia Derkach <mary.moss333@gmail.com>
2020-10-09 01:24:50 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+  if(n<0){
+      let r = String(n*(-1)).split('').reverse().join('');
+      return +r
+  }
+  else{
+    let r = String(n).split('').reverse()
+    if (r[0]===0){
+         r.splice(0, 1)
+         let s= r.join('')
+         return +s
+    }
+    else{
+        let z = r.join('')
+        return +z
+    }
+  }
 }
